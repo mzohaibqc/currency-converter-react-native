eabc813de671d39584b147e5dcd723688bc85a57
Zohaib Ijaz <mzohaib.qc@gmail.com>
2020-06-21 23:48:21 +0500

    Added Fontawesome icons Added gear icon (faCog) in Home screen header Added Home screen logo/image Modified Home page background color to white

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  */
 import 'react-native-gesture-handler';
 import React from 'react';
-
+import { View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -17,21 +17,28 @@ const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: '' }}
-        />
-        <Stack.Screen
-          name="Options"
-          component={Options}
-          options={{ title: '' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ title: '' }}
+          />
+          <Stack.Screen
+            name="Options"
+            component={Options}
+            options={{ title: '' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    
   );
 }
+
+const styles = StyleSheet.create({
+  navigation: {
+    backgroundColor: '#506d79'
+  }
+})
 
 export default App;
--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
+
+import logo from '../../assets/currency-logo.png';
 
 function Home({ navigation }) {
     return (
         <View style={styles.home}>
             <View style={styles.header}>
-                <Text
-                    onPress={() => navigation.navigate('Options')}
-                >Go To Options</Text>
+                <FontAwesomeIcon style={styles.optionsIcon} icon={ faCog } size={20} onPress={() => navigation.navigate('Options')}/>
             </View>
             <View style={styles.content}>
-                <Text>Home Content2</Text>
+              <View style={styles.logo}>
+                <Image source={logo} style={styles.logo} />
+              </View>
             </View>
         </View>
     )
@@ -24,11 +28,24 @@ Home.propTypes = {
 export default Home;
 
 const styles = StyleSheet.create({
-    home: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+    home: { flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' },
     header: {
         height: 20,
         marginTop: 40,
         alignSelf: 'flex-end',
+    },
+    header: {
+        height: 20,
+        marginTop: 10,
+        alignSelf: 'flex-end',
+    },
+    optionsIcon: {
+        marginRight: 10,
+        color: '#506d79',
+    },
+    logo: {
+        width: 250,
+        height: 250
     },
     content: {
         flex: 1,
